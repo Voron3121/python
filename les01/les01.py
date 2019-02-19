5ac6b3834e28a90dc59fe49e8c7377da9bde2d2f
Max <uchotki@mail.ru>
2019-02-19 23:08:45 +0300

    Написал ветвления

--- les01/les01.py
+++ les01/les01.py
@@ -2,7 +2,19 @@
 
 #Комментарии
 print("SUPER PROGRAMM!!!")
-
 print("Привет программист!")
 name = input("Ваше имя: ")
+
 print(name, ", добро пожаловать в мир Python!")
+
+answer = input("Давай поработаем? (Y/N)")
+
+
+#PEP-8
+if answer == 'Y':
+    print("Вам премия!")
+elif answer == 'N':
+    print("До свидания")
+else:
+    print("Неизвестный ответ")
+#pass - пустышка.
