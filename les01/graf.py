950a81fa97512b338de2ce0aef5e49d207058dca
Max <uchotki@mail.ru>
2019-02-21 20:37:16 +0300

    Доработан до барабана

--- les01/graf.py
+++ les01/graf.py
@@ -9,15 +9,18 @@ def gotoxy(x, y):
     turtle.goto(x, y)
     turtle.pendown()
 
+def draw_circle(color, r):
+    turtle.fillcolor(color)
+    turtle.begin_fill()
+    turtle.circle(r)
+    turtle.end_fill()
+
 turtle.speed(0)
 
 gotoxy(0, 0)
 turtle.circle(80)
 gotoxy(0, 165)
-turtle.fillcolor('red')
-turtle.begin_fill()
-turtle.circle(7)
-turtle.end_fill()
+draw_circle("red", 7)
 
 phi = 360 / 7
 r = 50
@@ -25,7 +28,12 @@ r = 50
 for i in range(0, 7):
     phi_rad = phi * i * math.pi / 180.0
     gotoxy(math.sin(phi_rad)*r, math.cos(phi_rad)*r + 57)
-    turtle.circle(21)
+    draw_circle("brown", 21)
+    draw_circle("white", 21)
+
+
+gotoxy(math.sin(phi_rad)*r, math.cos(phi_rad)*r + 57)
+draw_circle("brown", 21)
 
 answer = ''
 while answer != 'n':
